5072c31352e325c6a56324a09a5124d785cbcaed
Jurriaan <jurriaanr@gmail.com>
2025-01-22 23:56:48 +0100

    builder

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,12 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "rebate-uploader"
             packageVersion = "1.0.0"
+
+            windows {
+                menu = true
+                // see https://wixtoolset.org/documentation/manual/v3/howtos/general/generate_guids.html
+                upgradeUuid = "61DAB35E-17CB-43B0-81D5-B30E1C0830FA"
+            }
         }
     }
 }
