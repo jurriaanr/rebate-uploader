4fbb01fbe8f5c005985532cff721765de91136af
Jurriaan <jurriaanr@gmail.com>
2025-02-02 16:40:43 +0100

    wix uuid

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,14 +34,20 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "rebate-uploader"
             packageVersion = "1.0.2"
+            description = "Upload charge card CSV files to the online api"
+            copyright = "Copyright (c) 2025 Fastned"
+            vendor = "Fastned"
 
             macOS {
                 iconFile.set(project.file("src/main/resources/fastned.icns"))
             }
             windows {
-                menu = true
+                menu = false
+                menuGroup = "Fastned Rebate"
+                dirChooser = true
+                perUserInstall = true
                 // see https://wixtoolset.org/documentation/manual/v3/howtos/general/generate_guids.html
-                upgradeUuid = "61DAB35E-17CB-43B0-81D5-B30E1C0830FA"
+                upgradeUuid = "2F26204F-E07A-470E-A1D8-AE3D63A558AE"
                 iconFile.set(project.file("src/main/resources/fastned.ico"))
             }
             linux {
